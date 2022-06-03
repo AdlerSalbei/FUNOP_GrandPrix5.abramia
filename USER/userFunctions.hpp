36edc230669eabd14a6acaa59ba58b3b54968c5c
Salbei <salbei@gruppe-adler.de>
2022-06-03 16:53:18 +0200

    meh 2.0

--- USER/userFunctions.hpp
+++ USER/userFunctions.hpp
@@ -4,7 +4,15 @@
 */
 
 class GRAD_grandPrix {
-    tag = "GRAD_grandPrix";
+	tag = "grad_grandPrix";
 
+	#include "functions\points\cfgFunctions.hpp"
+	#include "functions\race\cfgFunctions.hpp"
+	#include "functions\water\cfgFunctions.hpp"
+	#include "functions\ZiG\cfgFunctions.hpp"
+  	#include "functions\OneShotWonder\CfgFunctions.hpp"
+  	#include "functions\P.U.P.S\CfgFunctions.hpp"
+	#include "functions\daredevil\CfgFunctions.hpp"
+	#include "functions\common\CfgFunctions.hpp"
     #include "functions\redLightGreenLight\CfgFunctions.hpp"
 };
